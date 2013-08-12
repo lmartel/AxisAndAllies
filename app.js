dae148dabec805417e477723ccc4d3ce3c187118
Leo <github.com/lmartel>
2013-08-12 01:09:04 -0700

    added image assets and one map; added accounts system and new game form; created Unit class

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 Games = new Meteor.Collection("games");
 Maps = new Meteor.Collection("maps");
+Units = new Meteor.Collection("units");
+
 Actions = new Meteor.Collection("actions");
 
-/* Begin classes */
-function Map(name, board){
-    this.name = name;
-    this.board = board;
-}
 
-function Terrain(name, bg){
-    /* TODO: somehow inject arbitrary rules through terrain */
-    /* Distance calculator asks each hex how much it costs? */
-    this.name = name;
-    this.bg = bg;
-}
 /* End classes */
+/* Helper functions used by constructors */
+function loadBasicUnitFields(unit, fields, options){
+
+}
+/* End constructor helpers */
 
 if (Meteor.isClient) {
+
+    Template.gameList.events({
+       'click .new-game-button': function(){
+           safeDOMEmpty(".content").append(Template.newGame);
+
+           $(".create-game").on("click", function(){
+               var input = formToHash(".new-game-form");
+               var opponent = Meteor.users.findOne({"emails.address": input["opponent"]});
+               if(!opponent){
+                   alert("No one's registered with that email address! Tell your friend at "
+                       + input["opponent"] + " to make an account so you can play.");
+                   return false;
+               } else opponent = opponent._id;
+               var name = input["name"],
+                   allies,
+                   axis,
+                   user = Meteor.userId();
+               if(input.faction === "Allies"){
+                   allies = user;
+                   axis = opponent;
+               } else {
+                   allies = opponent;
+                   axis = user;
+               }
+               // TODO: map selection
+               var map = Maps.findOne();
+               Games.insert(new Game(name, allies, axis, map));
+
+               //immediate TODO: get games that contain userid, render game by rendering map and then executing associated actions
+           });
+       }
+    });
 
     var board;
     var KLASS = "test";
@@ -47,7 +75,7 @@ if (Meteor.isClient) {
         // Set up board
 
         var BOARD = [
-            [1,-4],[0,1],[0,0, "http://placekitten.com/200/200"],[0,-1],[0,-3],[0,-2],[0,2],[0,3],[-1,4],[-2,4],
+            [1,-4],[0,1],[0,0],[0,-1],[0,-3],[0,-2],[0,2],[0,3],[-1,4],[-2,4],
             [-3,4],[-4,4],[-3,3],[-2,2],[-2,3],[-1,3],[-1,2],[-1,1],[-1,0],[-2,1],[-3,2],
             [1,1],[1,0],[2,-1],[1,-1],[-1,-1],[1,-2],[2,-2],[3,-2],[3,-3],[2,-3],[1,-3],
             [2,-4],[3,-4],[4,-4],[0,-4],[-1,-3],[-1,-2],[-2,-1],[-2,0],[-3,1],[-4,2],
@@ -58,7 +86,7 @@ if (Meteor.isClient) {
         board = (new H$.HexGrid(480, 420, 8, KLASS));
         //board.addMany(BOARD).drawAll();
         //board.addMany([ [-9,-3],[-9,-2],[-10,-1],[-10,0],[-11,1],[-11,2],[-12,3],[-8,-3],[-7,-3],[-11,3],[-10,3],[-9,2],[-8,1],[-7,-2],[-7,-1],[-7,0],[-8,3],[-6,1],[-7,2],[-5,0],[-4,-1],[-3,-2],[-2,-3],[-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2],[-2,3],[-5,1],[-4,1],[-3,1],[0,-3],[0,-2],[0,-1],[0,0],[0,1],[0,2],[0,3],[1,2],[2,1],[2,2],[2,3],[3,2],[4,1],[5,0],[6,-1],[7,-2],[8,-3],[2,0],[3,0],[4,3],[5,2],[6,1],[7,0],[8,-1],[9,-2],[10,-3],[10,-2],[10,-1],[10,0],[10,1],[10,2],[8,1],[7,1],[9,1] ]).drawAll();
-        board.addMany([ [-9,-3],[-9,-2],[-10,-1],[-10,0],[-11,1],[-11,2],[-12,3],[-8,-3],[-7,-3],[-11,3],[-10,3],[-9,2],[-8,1],[-7,-2],[-7,-1],[-7,0],[-8,3],[-6,1],[-7,2],[-5,0],[-4,-1],[-3,-2],[-2,-3],[-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2],[-2,3],[-5,1],[-4,1],[-3,1],[0,-3],[0,-2],[0,-1],[0,0],[0,1],[0,2],[0,3],[1,2],[2,1],[2,2],[2,3],[3,2],[4,1],[5,0],[6,-1],[7,-2],[8,-3],[2,0],[3,0],[4,3],[5,2],[6,1],[7,0],[8,-1],[9,-2],[10,-3],[10,-2],[10,-1],[10,0],[10,1],[10,2],[8,1],[7,1],[9,1] ]).drawAll();
+        board.addMany([ [-9,-3],[-9,-2],[-10,-1],[-10,0, "grass.jpg"],[-11,1],[-11,2],[-12,3],[-8,-3],[-7,-3],[-11,3],[-10,3],[-9,2],[-8,1],[-7,-2],[-7,-1],[-7,0],[-8,3],[-6,1],[-7,2],[-5,0],[-4,-1],[-3,-2],[-2,-3],[-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2],[-2,3],[-5,1],[-4,1],[-3,1],[0,-3],[0,-2],[0,-1],[0,0],[0,1],[0,2],[0,3],[1,2],[2,1],[2,2],[2,3],[3,2],[4,1],[5,0],[6,-1],[7,-2],[8,-3],[2,0],[3,0],[4,3],[5,2],[6,1],[7,0],[8,-1],[9,-2],[10,-3],[10,-2],[10,-1],[10,0],[10,1],[10,2],[8,1],[7,1],[9,1] ]).drawAll();
         // Render actions already in database
         Actions.find().forEach(function(action){
             renderAction(action);
@@ -86,7 +114,14 @@ if (Meteor.isClient) {
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        initializeMaps();
+        var RESEED = true;
+        if(RESEED){
+            Maps.remove({});
+            Seed.maps();
+
+            Units.remove({});
+            Seed.units();
+        }
 
         // Everything below here: test code
 
@@ -94,19 +129,5 @@ if (Meteor.isServer) {
 
         //var board = new H$.HexGrid(0, 0, 32, "foo");
     });
-
-    /* Begin private server helper functions */
-
-
-    function initializeMaps(){
-        var BOARD = [
-            [1,-4],[0,1],[0,0],[0,-1],[0,-3],[0,-2],[0,2],[0,3],[-1,4],[-2,4],
-            [-3,4],[-4,4],[-3,3],[-2,2],[-2,3],[-1,3],[-1,2],[-1,1],[-1,0],[-2,1],[-3,2],
-            [1,1],[1,0],[2,-1],[1,-1],[-1,-1],[1,-2],[2,-2],[3,-2],[3,-3],[2,-3],[1,-3],
-            [2,-4],[3,-4],[4,-4],[0,-4],[-1,-3],[-1,-2],[-2,-1],[-2,0],[-3,1],[-4,2],
-            [-4,3],[-5,4],[0,4],[1,3],[1,2],[2,1],[2,0],[3,-1],[4,-2],[4,-3],[5,-4]
-        ];
-        var villageSkirmish = new Map("Village Skirmish", BOARD);
-    }
 
 }
---
+++ server/seeds.js
@@ -0,0 +1,52 @@
+Seed = {};
+Seed.maps = function(){
+    var CLEAR = "grass.jpg";
+    var MARSH = "marsh.jpg";
+    var TOWN = "town.jpg";
+    var FOREST = "forest.jpg";
+    var HILL = "hill.jpg";
+    var POND = "pond.jpg";
+    var HALF_AND_HALF = [ [0,0,CLEAR],[0,1,CLEAR],[-1,2,CLEAR],[-1,3,CLEAR],[-2,4,CLEAR],[-2,5,CLEAR],[1,5,CLEAR],[2,5,CLEAR],
+        [3,5,CLEAR],[4,5,CLEAR],[5,5,CLEAR],[6,3,CLEAR],[7,1,CLEAR],[7,0,CLEAR],[9,-4,CLEAR],[10,-5,CLEAR],[9,-5,CLEAR],
+        [8,-5,CLEAR],[7,-5,CLEAR],[6,-5,CLEAR],[3,-5,CLEAR],[-3,5,CLEAR],[-4,5,CLEAR],[-7,5,CLEAR],[-8,5,CLEAR],
+        [-9,5,CLEAR],[-10,5,CLEAR],[-9,4,CLEAR],[-9,3,CLEAR],[-8,2,CLEAR],[-8,1,CLEAR],[-7,0,CLEAR],[-7,-1,CLEAR],
+        [-6,-2,CLEAR],[-6,-3,CLEAR],[-4,-5,CLEAR],[-3,-5,CLEAR],[0,-5,CLEAR],[-1,-5,CLEAR],[-6,0,CLEAR],[-5,0,CLEAR],
+        [-4,0,CLEAR],[-3,0,CLEAR],[-2,0,CLEAR],[-1,0,CLEAR],[-5,1,CLEAR],[-5,2,CLEAR],[-6,3,CLEAR],[-6,4,CLEAR],
+        [-7,4,CLEAR],[-5,4,CLEAR],[-3,4,CLEAR],[-3,3,CLEAR],[-4,3,CLEAR],[-5,3,CLEAR],[-4,2,CLEAR],[-4,1,CLEAR],
+        [-3,1,CLEAR],[-3,2,CLEAR],[-2,1,CLEAR],[-1,1,CLEAR],[-2,2,CLEAR],[-2,3,CLEAR],[0,3,CLEAR],[1,3,CLEAR],[1,4,CLEAR],
+        [2,4,CLEAR],[3,4,CLEAR],[4,4,CLEAR],[4,3,CLEAR],[5,2,CLEAR],[6,1,CLEAR],[1,0,CLEAR],[1,1,CLEAR],[0,2,CLEAR],
+        [1,2,CLEAR],[2,1,CLEAR],[2,0,CLEAR],[3,0,CLEAR],[4,0,CLEAR],[5,0,CLEAR],[6,0,CLEAR],[5,1,CLEAR],[4,1,CLEAR],
+        [3,1,CLEAR],[2,2,CLEAR],[3,2,CLEAR],[4,2,CLEAR],[3,3,CLEAR],[2,3,CLEAR],[5,3,MARSH],[6,2,MARSH],[0,5,TOWN],
+        [-7,1,TOWN],[-6,1,TOWN],[-6,2,TOWN],[-7,2,TOWN],[-8,3,TOWN],[-7,3,TOWN],[-8,4,TOWN],[-6,5,FOREST],
+        [-5,5,FOREST],[-4,4,FOREST],[5,4,FOREST],[-1,4,HILL],[0,4,HILL],[-1,5,HILL],[0,-1,CLEAR],[1,-2,CLEAR],
+        [1,-3,CLEAR],[2,-4,CLEAR],[-5,-4,FOREST],[-5,-3,FOREST],[-5,-2,FOREST],[-5,-5,FOREST],[1,-5,FOREST],[1,-4,FOREST],
+        [-2,-5,FOREST],[-2,-4,FOREST],[-2,-3,FOREST],[-2,-2,FOREST],[2,-5,HILL],[0,-2,TOWN],[-1,-1,TOWN],[-6,-1,CLEAR],
+        [-5,-1,CLEAR],[-4,-1,CLEAR],[-4,-2,CLEAR],[-4,-3,CLEAR],[-4,-4,CLEAR],[-3,-4,CLEAR],[-3,-3,CLEAR],
+        [-3,-2,CLEAR],[-3,-1,CLEAR],[-2,-1,CLEAR],[-1,-2,CLEAR],[-1,-3,CLEAR],[0,-3,CLEAR],[0,-4,CLEAR],[-1,-4,CLEAR],
+        [9,-3,TOWN],[8,-2,TOWN],[8,-1,TOWN],[8,-3,TOWN],[7,-1,TOWN],[6,-2,MARSH],[5,-1,MARSH],[4,-1,FOREST],
+        [2,-1,FOREST],[1,-1,FOREST],[4,-4,FOREST],[5,-5,FOREST],[4,-5,FOREST],[5,-2,POND],[6,-3,POND],[3,-4,CLEAR],
+        [2,-3,CLEAR],[2,-2,CLEAR],[3,-3,CLEAR],[4,-3,CLEAR],[3,-2,CLEAR],[3,-1,CLEAR],[4,-2,CLEAR],[5,-3,CLEAR],
+        [5,-4,CLEAR],[6,-4,CLEAR],[7,-4,CLEAR],[8,-4,CLEAR],[7,-3,CLEAR],[7,-2,CLEAR],[6,-1,CLEAR] ];
+    Maps.insert(new Map("Half and Half", HALF_AND_HALF));
+};
+
+Seed.units = function(){
+    function makeUnit(name, nationality, type, cost, speed, defense, soldierAttacks, vehicleAttacks){
+        Units.insert(new Unit({
+            name: name,
+            nationality: nationality,
+            type: type,
+            cost: cost,
+            speed: speed,
+            defense: defense,
+            soldierAttacks: soldierAttacks,
+            vehicleAttacks: vehicleAttacks
+        }));
+    }
+
+    makeUnit("M1 Garand Rifle", Faction.ALLIES, UnitType.SOLDIER, 4, 1, [4, 4], [8, 7], [2]);
+    makeUnit("M4A1 Sherman", Faction.ALLIES, UnitType.VEHICLE, 21, 4, [5, 4], [9, 9, 7], [13, 11, 9]);
+
+    makeUnit("Mauser Kar 98K", Faction.AXIS, UnitType.SOLDIER, 3, 1, [4, 4], [8, 6], [2]);
+    makeUnit("Panzer IV Ausf. G", Faction.AXIS, UnitType.VEHICLE, 30, 3, [5, 3], [7, 7, 6], [15, 13, 11]);
+};
