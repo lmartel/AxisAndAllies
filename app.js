63b30a782e445e2a6716f8930793cec2428ff4ff
Leo Martel <leo@lpm.io>
2013-08-12 20:28:57 -0700

    finished game creation/loading, basic game info and map renders

--- app.js
+++ app.js
@@ -4,56 +4,35 @@ Units = new Meteor.Collection("units");
 
 Actions = new Meteor.Collection("actions");
 
-
-/* End classes */
-/* Helper functions used by constructors */
-function loadBasicUnitFields(unit, fields, options){
-
-}
-/* End constructor helpers */
-
 if (Meteor.isClient) {
-
-    Template.gameList.events({
-       'click .new-game-button': function(){
-           safeDOMEmpty(".content").append(Template.newGame);
-
-           $(".create-game").on("click", function(){
-               var input = formToHash(".new-game-form");
-               var opponent = Meteor.users.findOne({"emails.address": input["opponent"]});
-               if(!opponent){
-                   alert("No one's registered with that email address! Tell your friend at "
-                       + input["opponent"] + " to make an account so you can play.");
-                   return false;
-               } else opponent = opponent._id;
-               var name = input["name"],
-                   allies,
-                   axis,
-                   user = Meteor.userId();
-               if(input.faction === "Allies"){
-                   allies = user;
-                   axis = opponent;
-               } else {
-                   allies = opponent;
-                   axis = user;
-               }
-               // TODO: map selection
-               var map = Maps.findOne();
-               Games.insert(new Game(name, allies, axis, map));
-
-               //immediate TODO: get games that contain userid, render game by rendering map and then executing associated actions
-           });
-       }
-    });
 
     var board;
     var KLASS = "test";
     var SELECT = "." + KLASS;
 
+    Template.gameList.events({
+       'click .new-game-button': displayGameForm(),
+       'click .game-link': function(){
+           displayGame(this);
+       }
+    });
+
+    Template.gameList.userGames = function(){
+        return Games.find({
+            $or:[
+                {"players.allies": Meteor.userId()},
+                {"players.axis": Meteor.userId()}
+            ]
+        });
+    };
+
+    Template.gameSummary.faction = getFactionString;
+
+    Template.game.faction = getFactionString;
+
     Template.board.events({
         'click': function(e){
             var coords = H$.Util.relativeCoordsFromClick(KLASS, e);
-            //console.log(coords);
             var hex = board.getAt(coords);
             if(hex !== null){
                 var action = hex.action().setBackgroundImage("http://placekitten.com/300/300").draw();
@@ -70,6 +49,69 @@ if (Meteor.isClient) {
         if(board) renderAction(this);
     };
 
+    /* Begin private client helper functions */
+
+    function renderAction(action){
+        board.action(action.document).$exec();
+    }
+
+    function createGameFromForm(){
+        var input = formToHash(".new-game-form");
+
+        Meteor.call("userLookup", input["opponent"], function(err, opponent){
+            if(err) throw(err);
+            if(!opponent){
+                alert("No one's registered with that email address! Tell your friend at "
+                    + input["opponent"] + " to make an account so you can play.");
+                return false;
+            }
+
+            var name = input["name"],
+                allies,
+                axis,
+                user = Meteor.userId();
+
+            if(user === opponent){
+                alert("You can't play with yourself! (At least make two accounts so we can keep track of who's who)");
+                return false;
+            }
+
+            if(input["faction"] === "Allies"){
+                allies = user;
+                axis = opponent;
+            } else {
+                allies = opponent;
+                axis = user;
+            }
+
+            // TODO: map selection
+            var map = Maps.findOne();
+            var game = new Game(name, allies, axis, map);
+            Games.insert(game);
+            displayGame(game);
+            return true;
+        });
+    }
+
+    function getFactionString(){
+        if(this.players.allies === Meteor.userId()) return Faction.ALLIES.name;
+        return Faction.AXIS.name;
+    }
+
+    function displayGameForm(){
+        safeDOMEmpty(".content").append(Template.newGame);
+        $(".create-game").on("click", createGameFromForm);
+    }
+
+    function displayGame(game){
+        safeDOMEmpty(".content").append(Template.game(game));
+        board = (new H$.HexGrid(480, 420, 28, KLASS)).addMany(game.map.layout).drawAll();
+        // immediate TODO: associate actions with game
+    }
+
+    /* Stubs of server methods */
+    Meteor.methods({});
+
     Meteor.startup(function(){
 
         // Set up board
@@ -82,7 +124,6 @@ if (Meteor.isClient) {
             [-4,3],[-5,4],[0,4],[1,3],[1,2],[2,1],[2,0],[3,-1],[4,-2],[4,-3],[5,-4]
         ];
         var BACKGROUND = "http://placekitten.com/100/100";
-        board = (new H$.HexGrid(480, 420, 28, KLASS));
         board = (new H$.HexGrid(480, 420, 8, KLASS));
         //board.addMany(BOARD).drawAll();
         //board.addMany([ [-9,-3],[-9,-2],[-10,-1],[-10,0],[-11,1],[-11,2],[-12,3],[-8,-3],[-7,-3],[-11,3],[-10,3],[-9,2],[-8,1],[-7,-2],[-7,-1],[-7,0],[-8,3],[-6,1],[-7,2],[-5,0],[-4,-1],[-3,-2],[-2,-3],[-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2],[-2,3],[-5,1],[-4,1],[-3,1],[0,-3],[0,-2],[0,-1],[0,0],[0,1],[0,2],[0,3],[1,2],[2,1],[2,2],[2,3],[3,2],[4,1],[5,0],[6,-1],[7,-2],[8,-3],[2,0],[3,0],[4,3],[5,2],[6,1],[7,0],[8,-1],[9,-2],[10,-3],[10,-2],[10,-1],[10,0],[10,1],[10,2],[8,1],[7,1],[9,1] ]).drawAll();
@@ -103,24 +144,20 @@ if (Meteor.isClient) {
 
     });
 
-    /* Begin private client helper functions */
-
-    function renderAction(action){
-        board.action(action.document).$exec();
-    }
-
 }
 
-// On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        var RESEED = true;
-        if(RESEED){
+        var RESET_DB = false;
+        if(RESET_DB){
             Maps.remove({});
             Seed.maps();
 
             Units.remove({});
             Seed.units();
+
+            Games.remove({});
+            Actions.remove({});
         }
 
         // Everything below here: test code
@@ -128,6 +165,17 @@ if (Meteor.isServer) {
         // Actions.remove({});
 
         //var board = new H$.HexGrid(0, 0, 32, "foo");
+    });
+
+    Meteor.methods({
+        /**
+         * Looks up whether a user exists without exposing the entire email database to the client.
+         */
+       userLookup: function(email){
+            var user = Meteor.users.findOne({"emails.address": email});
+            if(user) return user._id;
+            return undefined;
+       }
     });
 
 }
